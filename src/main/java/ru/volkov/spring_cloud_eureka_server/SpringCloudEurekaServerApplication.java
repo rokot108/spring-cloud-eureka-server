7667bce82aa7894aca3b515ccbaaff1291a87e36
rokot108 <38143263+rokot108@users.noreply.github.com>
2019-01-28 18:47:32 +0300

    Initial commit

---
+++ src/main/java/ru/volkov/spring_cloud_eureka_server/SpringCloudEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package ru.volkov.spring_cloud_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaServerApplication.class, args);
+    }
+
+}
+
